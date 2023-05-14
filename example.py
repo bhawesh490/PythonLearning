8c16baddacd8f27c65ea2779b44927560f6969e1
root <root@LAPTOP-K540UG7F>
2023-05-14 19:56:56 +0530

    learnt about environment variables

--- example.py
+++ example.py
@@ -4,3 +4,9 @@ def greet(name):
 print("What's your name?")
 name = input()
 greet(name)
+from dotenv.main import load_dotenv
+import os
+load_dotenv()
+favorite_language = os.environ['LANGUAGE']
+my_name = os.environ['MY_NAME']
+print (f'favorite language is {favorite_language} and my name is {my_name}')
